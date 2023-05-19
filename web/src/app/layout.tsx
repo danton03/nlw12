c7a793c934bfb039ab923c41d3dd4b55fdcaadb8
Danton Matheus <dantoncosta03@hotmail.com>
2023-05-19 12:13:13 -0300

    feat: :lipstick: implementa o layout da homepage

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,11 +1,12 @@
-import { ReactNode } from "react";
 import "./globals.css";
+import { ReactNode } from "react";
 import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from "next/font/google";
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
+
 const baiJamjuree = BaiJamjuree({
   subsets: ["latin"],
   weight: "700",
@@ -14,13 +15,16 @@ const baiJamjuree = BaiJamjuree({
 
 export const metadata = {
   title: "NLW Spacetime",
-  description: "Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript.",
+  description:
+    "Uma cápsula do tempo construída com React, Next.js, TailwindCSS e Typescript.",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-br">
-      <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans`}>
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
         {children}
       </body>
     </html>
